820fb4a6a993f7adc0549e6831ce620502e68d0c
Jpablots <juanpablo.bernal@udea.edu.co>
2026-05-21 23:17:35 -0500

    Primera parte del proyecto 'configuracion', lista

---
+++ src/main/java/com/proyectoTecnicas/resenas_peliculas/ResenasPeliculasApplication.java
@@ -0,0 +1,13 @@
+package com.proyectoTecnicas.resenas_peliculas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResenasPeliculasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResenasPeliculasApplication.class, args);
+	}
+
+}
